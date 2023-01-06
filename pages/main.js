bf117c2dc0133fb05ee579f11a650000b85a98ec
Sasha <hlupik69@gmail.com>
2023-01-06 03:27:14 +0300

    init: webpack

---
+++ pages/main.js
@@ -0,0 +1,4 @@
+import './main.css';
+import './test.css';
+console.log('ff');
+import { test } from "./test.js";
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const loader = require('sass-loader');
+
+const isDev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+	mode: 'development',
+	entry: './pages/main.js',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.ts$/i,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ],
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devtool: 'source-map',
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: './pages/main.html',
+            filename: 'main.html'
+		}),
+        new HTMLWebpackPlugin({
+			template: './pages/test.html',
+            filename: 'test.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		}),
+        // new CopyPlugin({
+        //     patterns: [
+        //         {
+        //             from: path.resolve(__dirname, './src/assets'),
+        //             to: path.resolve(__dirname, './dist/assets'),
+        //         },
+        //     ] 
+        // }),
+        new CleanWebpackPlugin(),
+	],
+}
