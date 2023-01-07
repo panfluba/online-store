4ea8f53a5ee56cbe6aaf1edb30c680bf1ab32054
Sasha <hlupik69@gmail.com>
2023-01-08 00:17:29 +0300

    feat: make cards with webpack

--- pages/main.js
+++ pages/main.js
@@ -1,1 +1,31 @@
 import './main.css';
+
+
+import products from './products.json'
+console.log(products);
+const mainTopic = document.querySelector('.main-topic');
+function createCards(productsArr) {
+    mainTopic.innerHTML = "";
+    productsArr.forEach(el => {
+        mainTopic.innerHTML += 
+        `
+        <div class="market-section">
+                    <div class="goods-section">
+                        <div class="goods-section__image">
+                            <img src="${el.images[0]}" alt="image-goods" class="goods__image">
+                        </div>
+                        <div class="goods-section__title">
+                            <div class="goods-information">
+                                <p class="goods__title">${el.title}</p>
+                                <p class="goods__brand">${el.brand}</p>
+                                <p class="goods__cost">${el.price}</p>
+                            </div>
+                            <div class="goods-icon__basket">
+                                <img src="../assets/icons/wbask.png" alt="basket" class="goods__basket">
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+    });
+}
+createCards(products);
