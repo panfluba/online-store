66aa9e0d3f0000b2349c459d0436a277710bc667
Sasha <hlupik69@gmail.com>
2023-01-08 20:21:39 +0300

    feat: add brand sort

--- pages/main.js
+++ pages/main.js
@@ -1,8 +1,13 @@
 import './main.css';
 
-
+import { test } from "./test";
+console.log(test);
 
 import products from './products.json'
+
+let newProductsArr = [...products];
+let checkedBrand = [];
+let checkedCategory ='';
 
 const mainTopic = document.querySelector('.main-topic');
 function createCards(productsArr) {
@@ -43,58 +48,89 @@ function generateBrandList() {
     filterBrand.innerHTML = `<p class="filter-brand__title filter__titles">Бренд</p>`
     createBrandList(products).forEach(el => {
         filterBrand.innerHTML += `
-        <label for="apple" class="label-apple">
-        <input type="checkbox" name="label-apple" id="label-apple" class="checkbox">
-            ${el}
+        <label for="label-brand" class="label-brand">
+        <input type="checkbox" name="label-brand" class="brand-input">
+            <span>${el}</span>
         </label>`
     });
 }
 generateBrandList()
-// console.log(createBrandList(products));
 
 ////////////////////////////////////
-const navList = document.querySelector('.nav-list');
-navList.addEventListener('click', function(e){
+const categoryList = document.querySelector('.nav-list');
+categoryList.addEventListener('click', function(e){
     let target = e.target;
     if (target.className === 'nav-list__item') {
-        let newArr = products.filter(el => el.category == target.lastElementChild.textContent);
-        createCards(newArr)
+        checkedCategory = target.lastElementChild.textContent;
 
     } else if (target.parentElement.className === 'nav-list__item') {
-        let newArr = products.filter(el => el.category == target.parentElement.lastElementChild.textContent);
-        createCards(newArr)
+        checkedCategory = target.parentElement.lastElementChild.textContent;
     }
+    allFilterFunc()
 })
+function checkCategory(arr) {
+    return checkedCategory ? arr.filter(el => el.category === checkedCategory) : arr;
+}
 
 // ЭТО ДЛЯ INPUT RANGE В ФИЛЬТРАХ
 
-window.onload = function(){
-    slideOne();
-    slideTwo();
+// window.onload = function(){
+//     slideOne();
+//     slideTwo();
+// }
+// let sliderOne = document.getElementById("slider-1");
+// let sliderTwo = document.getElementById("slider-2");
+// let displayValOne = document.getElementById("range-1");
+// let displayValTwo = document.getElementById("range-2");
+// let minGap = 0;
+// let sliderTrack = document.querySelector(".slider-track");
+// let sliderMaxValue = document.getElementById("slider-1").max;
+// function slideOne(){
+//     if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+//         sliderOne.value = parseInt(sliderTwo.value) - minGap;
+//     }
+//     displayValOne.textContent = sliderOne.value;
+//     fillColor();
+// }
+// function slideTwo(){
+//     if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+//         sliderTwo.value = parseInt(sliderOne.value) + minGap;
+//     }
+//     displayValTwo.textContent = sliderTwo.value;
+//     fillColor();
+// }
+// function fillColor(){
+//     percent1 = (sliderOne.value / sliderMaxValue) * 100;
+//     percent2 = (sliderTwo.value / sliderMaxValue) * 100;
+//     sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #3264fe ${percent1}% , #3264fe ${percent2}%, #dadae5 ${percent2}%)`;
+// }
+/////////////////////////////////////////////////
+const brandInputs = document.querySelectorAll('.brand-input');
+
+for (let i = 0; i < brandInputs.length; i++) {
+   
+    const brandInput = brandInputs[i];
+    brandInput.addEventListener('change',function(){
+        if (brandInput.checked) {
+            checkedBrand.push(brandInput.parentElement.lastElementChild.textContent);
+
+        } else {
+            checkedBrand = checkedBrand.filter(el => el != brandInput.parentElement.lastElementChild.textContent)
+        }
+        allFilterFunc()
+    })
+    
 }
-let sliderOne = document.getElementById("slider-1");
-let sliderTwo = document.getElementById("slider-2");
-let displayValOne = document.getElementById("range-1");
-let displayValTwo = document.getElementById("range-2");
-let minGap = 0;
-let sliderTrack = document.querySelector(".slider-track");
-let sliderMaxValue = document.getElementById("slider-1").max;
-function slideOne(){
-    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
-        sliderOne.value = parseInt(sliderTwo.value) - minGap;
-    }
-    displayValOne.textContent = sliderOne.value;
-    fillColor();
+function productsArrSortBrand() {
+    let productsArrSortBrand = newProductsArr.filter(el => checkedBrand.includes(el.brand));
+        // console.log(productsArrSortBrand);
+        // console.log(checkedBrand.length ? productsArrSortBrand : newProductsArr);
+    return checkedBrand.length ? productsArrSortBrand : newProductsArr;
 }
-function slideTwo(){
-    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
-        sliderTwo.value = parseInt(sliderOne.value) + minGap;
-    }
-    displayValTwo.textContent = sliderTwo.value;
-    fillColor();
-}
-function fillColor(){
-    percent1 = (sliderOne.value / sliderMaxValue) * 100;
-    percent2 = (sliderTwo.value / sliderMaxValue) * 100;
-    sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #3264fe ${percent1}% , #3264fe ${percent2}%, #dadae5 ${percent2}%)`;
+function allFilterFunc() {
+    let arr = productsArrSortBrand();
+    console.log(arr);
+    let arr1 = checkCategory(arr);
+    console.log(arr1);
+    createCards(arr1);
 }
---
+++ pages/test.js
@@ -0,0 +1,2 @@
+let test = 'test'
+export {test}
